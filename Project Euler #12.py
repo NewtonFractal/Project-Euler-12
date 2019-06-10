6da2aa6f80dd7c47ae99b74c9c59f7aa0f4ef7be
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-06-10 10:44:05 +0200

    Update Project Euler #12.py

--- Project Euler #12.py
+++ Project Euler #12.py
@@ -4,13 +4,14 @@ sums = [1]
 primelist = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
 
 def triangle_number(w):
-    numbers = [1]
     for y in range(2,w+1):
-        sums.append(y+sum(numbers))
-        numbers.append(y)
+        sums.append(int(((y+1)*y)/2))
     print(sums)
 
-triangle_number(12)
+triangle_number(10000)
+
+end = time.time()
+print(end - start)
 
 def triangle_divisors(LCM):
     for x in sums:

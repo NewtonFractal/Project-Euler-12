c5b97c593010bea34eb1174c0a4e3e06a9438be8
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-06-10 09:53:42 +0200

    Update Project Euler #12.py

--- Project Euler #12.py
+++ Project Euler #12.py
@@ -1,17 +1,13 @@
 import time
 start = time.time()
-sums = []
+sums = [1]
+primelist = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
 
 def triangle_number(w):
-    numbers = []
-    for y in range(1,w):
-            for x in range(1,y+1):
-                numbers.append(x)
-            sums.append((sum(numbers)))
-            numbers.clear()
+    numbers = [1]
+    for y in range(2,w+1):
+        sums.append(y+sum(numbers))
+        numbers.append(y)
     print(sums)
 
-triangle_number(10)
-
-end = time.time()
-print(end - start)
+triangle_number(12)

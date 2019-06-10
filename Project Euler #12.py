a4574f30eb19ceabbc2b2ff8680f9e7f6c16c0dd
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-06-10 14:42:46 +0200

    Update Project Euler #12.py

--- Project Euler #12.py
+++ Project Euler #12.py
@@ -1,26 +1,37 @@
 import time
 import math
+
 start = time.time()
 sums = []
 divisors = []
 
-def triangle_number(w):
-    for y in range(2,w+1):
-        sums.append(int(((y+1)*y)/2))
-    print(sums)
 
-triangle_number(50)
+def triangle_number(w):
+    for y in range(2, w + 1):
+        sums.append(int(((y + 1) * y) / 2))
+
+
+triangle_number(1000000)
 
 end = time.time()
 print(end - start)
 
+
 def Highly_divisible_triangular_number():
+    Done = False
     for x in sums:
-        for y in range(2,x):
+        for y in range(2, x):
             if x % y == 0:
                 divisors.append(y)
-             
+                if len(divisors) == 498:
+                    Done = True
+                    print(x)
+                    break
+        divisors.clear()
+        if Done == True:
+            break
+
+
 Highly_divisible_triangular_number()
-print(Factors)
 end = time.time()
 print(end - start)

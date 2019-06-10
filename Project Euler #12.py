7e35c5dad9fa78391ef0996bbbd8831546cae75b
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-06-10 18:30:15 +0200

    Update Project Euler #12.py

--- Project Euler #12.py
+++ Project Euler #12.py
@@ -1,37 +1,57 @@
 import time
 import math
-
+primelist = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
+primelist2 = [3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
+Factors = []
 start = time.time()
 sums = []
-divisors = []
+
 
 
 def triangle_number(w):
-    for y in range(100, w + 1):
+    for y in range(1, w + 1):
         sums.append(int(((y + 1) * y) / 2))
 
 
-triangle_number(1000000)
+triangle_number(2)
 
-end = time.time()
-print(end - start)
-
-
-def Highly_divisible_triangular_number():
-    Done = False
-    for x in sums:
-        for y in range(2, x):
-            if x % y == 0:
-                divisors.append(y)
-                if len(divisors) == 498:
-                    Done = True
-                    print(x)
-                    break
-        divisors.clear()
-        if Done == True:
+def Highly_divisible_triangular_number(number):
+    for x in primelist:
+        factor = True
+        while factor == True:
+            if number % x != 0:
+                factor = False
+                break
+            Factors.append(x)
+            number = int(number/x)
+        if x > number+ 1:
             break
+    for y in range(103, number + 1, 2):
+        prime = True
+        for x in primelist2:
+            if y % x == 0:
+                prime = False
+                break
+            if x > math.sqrt(y):
+                break
+        if prime == True:
+            primelist2.append(y)
+            factor2 = True
+            while factor2 == True:
+                if number % y != 0:
+                    factor2 = False
+                    break
+                Factors.append(y)
+                number = number / y
+                if y > number + 1:
+                    break
+            if y > number + 1:
+                break
+        if y > number + 1:
+            break            
 
-
-Highly_divisible_triangular_number()
+for x in sums:
+    Highly_divisible_triangular_number(x)
+print(Factors)
 end = time.time()
 print(end - start)

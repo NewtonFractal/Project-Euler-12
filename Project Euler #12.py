30f92e13c84332a5c1ea458b7e9e50c4903266de
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-06-10 14:24:09 +0200

    Update Project Euler #12.py

--- Project Euler #12.py
+++ Project Euler #12.py
@@ -1,10 +1,8 @@
 import time
 import math
 start = time.time()
-sums = [1]
-Factors = []
-primelist = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
-primelist2 = [3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
+sums = []
+divisors = []
 
 def triangle_number(w):
     for y in range(2,w+1):
@@ -17,41 +15,11 @@ end = time.time()
 print(end - start)
 
 def Highly_divisible_triangular_number():
-    for z in sums:
-        for x in primelist:
-            factor = True
-            while factor == True:
-                if z % x != 0:
-                    factor = False
-                    break
-                Factors.append(x)
-                z = int(z/x)
-            if x > z+ 1:
-                break
-        for y in range(103, z + 1, 2):
-            prime = True
-            for x in primelist2:
-                if y % x == 0:
-                    prime = False
-                    break
-                if x > math.sqrt(y):
-                    break
-            if prime == True:
-                primelist2.append(y)
-                factor2 = True
-                while factor2 == True:
-                    if z % y != 0:
-                        factor2 = False
-                        break
-                    Factors.append(y)
-                    z = number / y
-                    if y > z + 1:
-                        break
-                if y > z + 1:
-                    break
-            if y > z+ 1:
-                break
-
+    for x in sums:
+        for y in range(2,x):
+            if x % y == 0:
+                divisors.append(y)
+             
 Highly_divisible_triangular_number()
 print(Factors)
 end = time.time()

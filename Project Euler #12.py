3a4aca58b15c0ff785e49d41a636c3384bb75cd5
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-06-21 20:14:36 +0200

    Update Project Euler #12.py

--- Project Euler #12.py
+++ Project Euler #12.py
@@ -1,55 +1,28 @@
-import time
 import math
-primelist = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
-primelist2 = [3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
-Factors = []
+import time
+
 start = time.time()
-sums = []
-
-def triangle_number(w):
-    for y in range(1, w + 1):
-        sums.append(int(((y + 1) * y) / 2))
-
-
-triangle_number(12500)
+Answer =[]
 
 def Highly_divisible_triangular_number(number):
-    for x in primelist:
-        factor = True
-        while factor == True:
-            if number % x != 0:
-                factor = False
-                break
-            Factors.append(x)
-            number = int(number/x)
-        if x > number+ 1:
+    factors = []
+    for x in range(1, int(math.sqrt(number)+1)):
+        if number % x == 0:
+            factors.append(number / x)
+            factors.append(x)
+        if len(factors) > 500:
+            Answer.append(number)
+            print(number)
             break
-    for y in range(103, number + 1, 2):
-        prime = True
-        for x in primelist2:
-            if y % x == 0:
-                prime = False
-                break
-            if x > math.sqrt(y):
-                break
-        if prime == True:
-            primelist2.append(y)
-            factor2 = True
-            while factor2 == True:
-                if number % y != 0:
-                    factor2 = False
-                    break
-                Factors.append(y)
-                number = number / y
-                if y > number + 1:
-                    break
-            if y > number + 1:
-                break
-        if y > number + 1:
-            break            
 
-for x in sums:
-    Highly_divisible_triangular_number(x)
-print(Factors)
+
+def Triangle_number_generator(upper_bound):
+    for x in range(1, upper_bound):
+        if len(Answer) == 1:
+            break
+        Highly_divisible_triangular_number((x * (x + 1)) / 2)
+
+Triangle_number_generator(100000)
+
 end = time.time()
 print(end - start)

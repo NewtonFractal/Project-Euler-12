c19172eae0534283e0f69e58f25d4848e03b4eba
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-06-22 08:09:37 +0200

    Update Project Euler #12.py

--- Project Euler #12.py
+++ Project Euler #12.py
@@ -7,7 +7,7 @@ def Highly_divisible_triangular_number(number):
     factors = [1]
     for x in range(2, int(math.sqrt(number)+1)):
         if number % x == 0:
-            factors.append(number / x)
+            factors.append(number/x)
             factors.append(x)
     if len(factors) > 500:
         Answer.append(number)
@@ -17,7 +17,7 @@ def Triangle_number_generator(lower_bound,upper_bound):
     for x in range(lower_bound, upper_bound):
         if len(Answer) == 1:
             break
-        Highly_divisible_triangular_number((x * (x + 1)) / 2)
+        Highly_divisible_triangular_number((x *(x+1))/2)
 
 def lower_bound_finder(sum):
     Triangle_number_generator(int(((math.sqrt(8*sum+1)-1)/2)),34673462)
